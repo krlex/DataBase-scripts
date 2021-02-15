3b15fae0336e004ce7073cb97be6202fe7d4e9c7
Krstan Vjestica <krle@tilda.center>
2021-02-15 14:34:11 +0100

    setup form for creating new database

--- mariadb-create.sh
+++ mariadb-create.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
 
+if [ -z $1 ]; then
+  echo "Enter the Dataname:"
+  read DATANAME
+else
+  DATANAME=$1
+fi
+
+if [ -z $1 ]; then
+  echo "Enter the Username:"
+  read USERNAME
+else
+  USERNAME=$1
+fi
+
+stty -echo ; read -p "Enter New Password: " PASSWD; stty echo
+
 green() {
-  echo -e '\e[32m'$1'\e[m';
+  echo -e '\e[32m'$DATANAME'\e[m';
 }
 
-readonly EXPECTED_ARGS=3
 readonly E_BADARGS=65
 readonly MYSQL=`which mysql`
 
 # Construct the MySQL query
-readonly Q1="CREATE DATABASE IF NOT EXISTS $1;"
-readonly Q2="GRANT ALL ON *.* TO '$2'@'localhost' IDENTIFIED BY '$3';"
+readonly Q1="CREATE DATABASE IF NOT EXISTS $DATANAME;"
+readonly Q2="GRANT ALL ON *.* TO '$USERNAME'@'localhost' IDENTIFIED BY '$PASSWD';"
 readonly Q3="FLUSH PRIVILEGES;"
 readonly SQL="${Q1}${Q2}${Q3}"
-
-# Do some parameter checking and bail if bad
-if [ $# -ne $EXPECTED_ARGS ]
-then
-  echo "Usage: $0 dbname dbuser dbpass"
-  exit $E_BADARGS
-fi
 
 # Run the actual command
 $MYSQL -uroot -e "$SQL"
@@ -28,4 +36,4 @@ $MYSQL -uroot -e "$SQL"
 # $MYSQL -uroot -p -e "$SQL"
 
 # Let the user know the database was created
-green "Database $1 and user $2 created with a password you chose"
+green "Database $DATANAME and user $USERNAME created with a password you chose"
